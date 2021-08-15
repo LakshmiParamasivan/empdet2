9c36ce297d1acb1a4e62f3cb026d658d0de4a3a5
LakshmiParamasivan <t.ramyalakshmi@gmail.com>
2021-08-15 08:37:49 -0400

    added a comment -composite key simulation Annotation also added

--- empdet2/src/main/java/com/example/empdet2/Entity/EmployeeId.java
+++ empdet2/src/main/java/com/example/empdet2/Entity/EmployeeId.java
@@ -5,15 +5,17 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import java.io.Serial;
 import java.io.Serializable;
 @Embeddable
 @Getter
 @Setter
 public class EmployeeId implements Serializable {
-
+ @Serial
   private static final long serialVersionUID = 7778440275789187201L;
   private long employeeId;
 
   @Column(name = "Emp_Name")
     private String employeeName;
+  //code for composite key
 }
